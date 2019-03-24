1da3efc03a1cec09d6dd3222b8222f960641ba2c
Nikhil Prakash Patil <nikhilpatil.patil0@gmail.com>
2019-03-24 13:32:27 +0530

    guessing game

--- guessing_game.py
+++ guessing_game.py
@@ -4,7 +4,7 @@ print(num)
 a  = int(input("Enter a number :"))
 if a in range(1,100):
     if a in range(num-10,num+11):
-        print("WARM :"+str(num))
+        print("WARM : "+str(num))
     else:
         print("COLD :"+str(num))
 else:
