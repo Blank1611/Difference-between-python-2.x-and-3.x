c4f4f75d8bda0ac0dbc02742f2309c5a768b23da
Nikhil Prakash Patil <nikhilpatil@ieee.org>
2019-04-08 01:30:53 +0530

    Create seating_arrangement.py
    A practice challenge on hackerearth for finding the seat opposite to given seat and the seat type (WS,AS,MS).
    Link to challenge:
    https://www.hackerearth.com/practice/basic-programming/input-output/basics-of-input-output/practice-problems/algorithm/seating-arrangement-1/

---
+++ seating_arrangement.py
@@ -0,0 +1,47 @@
+def set_calc(x,M):
+    for i in range (M):
+        r=(x[i]%6)
+        if (r==0):
+            if  opp_no(x[i]):
+                print(x[i]-11, "WS", sep = " ")
+            else:
+                print(x[i]+1, "WS", sep = " ")
+        elif (r==1):
+            if opp_no(x[i]):
+                print(x[i]+11, "WS", sep = " ")
+            else:
+                print(x[i]-1, "WS", sep = " ")
+        elif (r==4):
+            if  opp_no(x[i]):
+                print(x[i]+5, "AS", sep = " ")
+            else:
+                print(x[i]-7, "AS", sep = " ")
+        elif (r==3):
+            if  opp_no(x[i]):
+                print(x[i]+7, "AS", sep = " ")
+            else:
+                print(x[i]-5, "AS", sep = " ")
+        elif (r==2):
+            if  opp_no(x[i]):
+                print(x[i]+9, "MS", sep = " ")
+            else:
+                print(x[i]-3, "MS", sep = " ")
+        elif (r==5):
+            if  opp_no(x[i]):
+                print(x[i]+3, "MS", sep = " ")
+            else:
+                print(x[i]-9, "MS", sep = " ")
+def opp_no(oe):
+    if (oe//6)%2==0:
+        return True
+    else:
+        return False
+    
+          
+    
+N = int(input())
+b=[]
+for i in range (N):
+    a=int(input())
+    b.append(a)
+set_calc(b,N)
